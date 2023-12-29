413dfd8fb50f15fdb50eb7b1ef81ce4cfb3437e8
junseo <djawnstj44@naver.com>
2023-12-29 10:05:22 +0900

    애플리케이션

---
+++ src/main/kotlin/com/djawnstj/jwt/common/config/ApplicationConfig.kt
@@ -0,0 +1,39 @@
+package com.djawnstj.jwt.common.config
+
+import com.djawnstj.jwt.user.repository.UserQueryRepository
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+
+@Configuration
+class ApplicationConfig(
+    private val userRepository: UserQueryRepository
+) {
+
+    // TODO 익셉션 고민 필요
+    @Bean
+    fun userDetailsService(): UserDetailsService = UserDetailsService {
+        userRepository.findByLoginId(it) ?: throw UsernameNotFoundException("User Not Found")
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+
+    @Bean
+    fun authenticationProvider(): AuthenticationProvider =
+        DaoAuthenticationProvider().apply {
+            setUserDetailsService(userDetailsService())
+            setPasswordEncoder(passwordEncoder())
+        }
+
+    @Bean
+    fun authenticationManager(config: AuthenticationConfiguration): AuthenticationManager = config.authenticationManager
+
+}
