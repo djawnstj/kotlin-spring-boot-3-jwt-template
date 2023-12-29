1195c9ec0c1fb4e1daacdde209ce2ac0dc30ce2d
junseo <djawnstj44@naver.com>
2023-12-29 10:05:32 +0900

    시큐리티 설정

---
+++ src/main/kotlin/com/djawnstj/jwt/auth/config/SecurityConfig.kt
@@ -0,0 +1,53 @@
+package com.djawnstj.jwt.auth.config
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.security.web.authentication.logout.LogoutHandler
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@EnableConfigurationProperties(JwtProperties::class)
+class SecurityConfig(
+    private val jwtAuthFilter: JwtAuthenticationFilter,
+    private val authenticationProvider: AuthenticationProvider,
+    private val logoutHandler: LogoutHandler
+) {
+
+    private companion object {
+        private val WHITE_LIST_URLS = arrayOf("/api/v1/auth/**", "/h2-console/**", "/error")
+    }
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain =
+        http.run {
+            csrf { it.disable() }
+            headers { headers ->
+                headers.frameOptions { it.disable() }
+            }
+            authorizeHttpRequests {
+                it.requestMatchers(*WHITE_LIST_URLS).permitAll()
+                    .anyRequest()
+                    .authenticated()
+            }
+            sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            authenticationProvider(authenticationProvider)
+            addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter::class.java)
+            logout {
+                it.logoutUrl("/api/v1/auth/logout")
+                    .addLogoutHandler(logoutHandler)
+                    .logoutSuccessHandler { req, res, auth -> SecurityContextHolder.clearContext() }
+            }
+            build()
+        }
+
+}
